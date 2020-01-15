b8b4d946784ea603e22a2c9f0cdfde17bd6ce1c4
RAVIKIM <artiveloper@gmail.com>
2020-01-15 22:58:13 +0900

    H2 반영

--- eatgo-admin-api/src/main/java/com/example/eatgo/interfaces/MenuItemController.java
+++ eatgo-admin-api/src/main/java/com/example/eatgo/interfaces/MenuItemController.java
@@ -4,18 +4,21 @@ import com.example.eatgo.domain.MenuItem;
 import com.example.eatgo.service.MenuItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MenuItemController {
 
     private final MenuItemService menuItemService;
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public ResponseEntity<?> list(@PathVariable Long restaurantId) {
+        List<MenuItem> menuItems = menuItemService.getMenuItems(restaurantId);
+        return ResponseEntity.ok().body(menuItems);
+    }
 
     @PatchMapping("/restaurants/{restaurantId}/menuitems")
     public ResponseEntity<?> bulkUpdate(@PathVariable Long restaurantId, @RequestBody List<MenuItem> menuItems) {
